ecc2cdfb2e24ca471a300cfc8fc6496878219d6d
NoRainLand <wuxinrufeng@qq.com>
2024-02-21 16:11:01 +0800

    commit message

---
+++ source/js/bootstrap.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	Theme.backToTop.register();
+	Theme.getDate.register();
+	Theme.getArchiveDate.register();
+	Theme.ranVerse.register();
+	// Theme.floatTags.register();
+	Theme.addHeimuTitle.register();
+	Miku.console.register();
+});
